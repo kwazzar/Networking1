adee32eb31b8c38250bdf4801f2cf7a15b3e18eb
kwazzar <fugitiwe@gmail.com>
2023-04-26 22:01:35 +0300

    Get , Post request. JSON decoder

--- NetWorking1/NetWorking1/CoursesViewController.swift
+++ NetWorking1/NetWorking1/CoursesViewController.swift
@@ -22,8 +22,8 @@ class CoursesViewController: UIViewController {
          
 //        let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_course"
 //        let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_courses"
-//        let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_website_description"
-        let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_missing_or_wrong_fields"
+       let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_website_description"
+//        let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_missing_or_wrong_fields"
         
         
         guard let url = URL(string: jsonUrlString) else { return }
