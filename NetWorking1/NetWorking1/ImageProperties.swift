c58006a8f827002a580595164c3f3f02e70ab939
Nazar Vesnych <99490824+kwazzar@users.noreply.github.com>
2023-08-09 03:37:51 +0300

    34 (#12)
    * refactory check
    
    * Upload image
    
    * keys fix
    
    * keys fix
    
    * Delete TEST/TestApp directory

---
+++ NetWorking1/NetWorking1/ImageProperties.swift
@@ -0,0 +1,20 @@
+//
+//  ImageProperties.swift
+//  NetWorking1
+//
+//  Created by Quasar on 09.05.2023.
+//
+
+import UIKit
+
+struct ImageProperties {
+    
+    let key: String
+    let data: Data
+    
+    init?(withImage image: UIImage, forKey key: String) {
+        self.key = key
+        guard let data = image.pngData() else { return nil }
+        self.data = data
+    }
+}
